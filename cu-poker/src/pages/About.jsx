04505ed7cbbac82634b6df3ca424642482e8a3d6
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2025-02-04 23:28:16 -0500

    mobile responsiveness

--- cu-poker/src/pages/About.jsx
+++ cu-poker/src/pages/About.jsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="w-full text-text">
       {/* Mission */}
-      <section className="min-h-screen w-full flex flex-col items-center justify-center text-center px-6 bg-primary text-white">
+      <section className="min-h-screen w-full flex flex-col items-center justify-center text-center px-6 bg-blue-500 text-white">
         <h1 className="text-4xl sm:text-7xl font-extrabold">Mission</h1>
         <p className="text-xl sm:text-3xl max-w-4xl mt-6">
           Our mission is to foster a community of poker enthusiasts at Columbia
@@ -12,7 +12,7 @@ function About() {
       </section>
 
       {/* History */}
-      <section className="min-h-screen w-full flex flex-col items-center justify-center text-center px-6 bg-secondary text-text">
+      <section className="min-h-screen w-full flex flex-col items-center justify-center text-center px-6 bg-blue-300 text-text">
         <h1 className="text-4xl sm:text-7xl font-extrabold">History</h1>
         <p className="text-xl sm:text-3xl max-w-4xl mt-6">
           Established in 2022, the Columbia Poker Club started as a small
@@ -23,52 +23,42 @@ function About() {
       </section>
 
       {/* Board */}
-      <section className="min-h-screen w-full flex flex-col items-center justify-center px-6 py-10 bg-accent text-text">
+      <section className="min-h-screen w-full flex flex-col items-center justify-center px-6 py-10 bg-blue-100 text-text">
         <h1 className="text-4xl sm:text-7xl font-extrabold text-center">
           Current Board
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10 w-full max-w-5xl">
+        <div className="flex flex-wrap justify-center gap-8 mt-10 w-full max-w-5xl">
           {[
-            { name: "Andrew Fahey", role: "Co-President" },
-            { name: "David Eyal", role: "Co-President" },
-            { name: "Jonah Aden", role: "Vice President" },
-            { name: "Juliana Shepard", role: "Treasurer" },
-            { name: "Nicolai Sotrop", role: "Secretary" },
-            { name: "Zachary Klein", role: "Outreach Chair" },
-            { name: "Lucy Rose", role: "Social Media Chair" },
-            { name: "Liz Hu", role: "Senior Advisor" },
-            { name: "Bailey Ng", role: "Senior Advisor" },
+            { name: "Andrew Fahey", role: "Co-President", class: "CC '25", major: "Political Science & Statistics" },
+            { name: "David Eyal", role: "Co-President", class: "CC '26", major: "Computer Science and Statistics" },
+            { name: "Jonah Aden", role: "Vice President", class: "CC '26", major: "Computer Science and PoliSci - Stats" },
+            { name: "Juliana Shepard", role: "Treasurer", class: "Barnard '26", major: "Business" },
+            { name: "Nicolai Sotrop", role: "Secretary", class: "CC '27", major: "Engineering" },
+            { name: "Zachary Klein", role: "Outreach Chair", class: "CC '27", major: "Political Science" },
+            { name: "Lucy Rose", role: "Social Media Chair", class: "'25", major: "Art History" },
+            { name: "Liz Hu", role: "Senior Advisor", class: "CC '25", major: "Biology" },
+            { name: "Bailey Ng", role: "Senior Advisor", class: "GS '25", major: "Financial Economics" },
+            { name: "Noah Krever", role: "Alumni Advisor", class: "'23?", major: "Econ?" },
+            { name: "Mateo Maturana", role: "Alumni Advisor", class: "'23?", major: "CS?" }
           ].map((member, index) => (
             <div
               key={index}
-              className="bg-white bg-opacity-20 p-6 rounded-lg text-center shadow-md flex flex-col items-center justify-center h-auto"
+              className="bg-white bg-opacity-20 p-6 rounded-lg text-center shadow-md flex flex-col items-center justify-center h-[250px] w-full md:w-[300px]"
             >
               <h3 className="text-2xl sm:text-4xl font-bold">{member.name}</h3>
               <p className="text-base sm:text-lg text-gray-700">
                 {member.role}
               </p>
+              <p className="text-base sm:text-lg text-gray-700">
+                {member.major}
+              </p>
+              <p className="text-base sm:text-lg text-gray-700">
+                {member.class}
+              </p>
+              
             </div>
           ))}
-
-          <div className="md:col-span-3 flex flex-col md:flex-row justify-center gap-8 w-full">
-            {[
-              { name: "Noah Krever", role: "Alumni Advisor" },
-              { name: "Mateo Maturana", role: "Alumni Advisor" },
-            ].map((member, index) => (
-              <div
-                key={index}
-                className="bg-white bg-opacity-20 p-6 rounded-lg text-center shadow-md flex flex-col items-center justify-center w-full md:w-[300px] h-auto"
-              >
-                <h3 className="text-2xl sm:text-4xl font-bold">
-                  {member.name}
-                </h3>
-                <p className="text-base sm:text-lg text-gray-700">
-                  {member.role}
-                </p>
-              </div>
-            ))}
-          </div>
         </div>
       </section>
     </div>
--- cu-poker/src/pages/Home.jsx
+++ cu-poker/src/pages/Home.jsx
@@ -16,31 +16,31 @@ function Home() {
           backgroundSize: "cover",
         }}
       >
-        <div className="w-[80%] h-[350px] flex flex-col items-center p-6 shadow-xl rounded-xl">
+        <div className="w-[90%] md:w-[80%] h-[300px] md:h-[350px] flex flex-col items-center p-4 md:p-6 shadow-xl rounded-xl">
           <img
             src={Logo}
             alt="Columbia Poker Club Logo"
-            className="w-[170px] h-[170px] mb-4 rounded-full"
+            className="w-[120px] md:w-[170px] h-[120px] md:h-[170px] mb-4 rounded-full"
           />
-          <h1 className="text-[90px] text-text text-center">
+          <h1 className="text-[50px] md:text-[90px] text-text text-center">
             Columbia Poker Club
           </h1>
         </div>
       </div>
 
       {/* About */}
-      <section className="w-full bg-white py-20 px-10 flex flex-col items-center">
-        <div className="max-w-5xl flex flex-col md:flex-row items-center">
-          <div className="w-full md:w-1/2 p-6">
-            <h2 className="text-6xl font-bold text-primary mb-4">About Us</h2>
-            <p className="text-3xl text-gray-700">
+      <section className="w-full bg-white py-10 md:py-20 px-5 md:px-10 flex flex-col items-center">
+        <div className="max-w-3xl md:max-w-5xl flex flex-col md:flex-row items-center">
+          <div className="w-full md:w-1/2 p-4 md:p-6">
+            <h2 className="text-4xl md:text-6xl font-bold text-primary mb-4">About Us</h2>
+            <p className="text-xl md:text-3xl text-gray-700">
               Columbia Poker Club brings together poker players for games,
-              workshops, and competitions. Whether you’re a beginner or a
-              seasoned pro, you’ll find a welcoming community here.
+              workshops, and competitions. Whether you're a beginner or a
+              seasoned pro, you'll find a welcoming community here.
             </p>
           </div>
 
-          <div className="w-full md:w-1/2 p-6">
+          <div className="w-full md:w-1/2 p-4 md:p-6">
             <img
               src={Table}
               alt="Poker Table"
@@ -51,48 +51,48 @@ function Home() {
       </section>
 
       {/* Upcoming Events */}
-      <section className="w-full bg-gray-100 py-20 px-10">
-        <div className="max-w-[80%] mx-auto text-center">
-          <h2 className="text-6xl font-bold text-primary mb-6">
+      <section className="w-full bg-gray-100 py-10 md:py-20 px-5 md:px-10">
+        <div className="max-w-[90%] md:max-w-[80%] mx-auto text-center">
+          <h2 className="text-4xl md:text-6xl font-bold text-primary mb-4 md:mb-6">
             Upcoming Events
           </h2>
-          <p className="text-3xl text-gray-600 mb-10">
+          <p className="text-xl md:text-3xl text-gray-600 mb-6 md:mb-10">
             Join our upcoming poker tournaments and club meetups!
           </p>
 
           {/* Grid of Events */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="bg-white shadow-lg p-20 rounded-lg border border-gray-200">
-              <h3 className="text-2xl font-bold text-secondary mb-2">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
+            <div className="bg-white shadow-lg p-10 md:p-20 rounded-lg border border-gray-200">
+              <h3 className="text-xl md:text-2xl font-bold text-secondary mb-2">
                 Club Freeplay
               </h3>
-              <p className="text-gray-700 text-xl">Next Thursday - 8:30 PM</p>
-              <p className="text-gray-700 text-xl">Uris Hall - 3rd Floor</p>
-              <p className="text-xl text-gray-500 mt-2">
+              <p className="text-gray-700 text-lg md:text-xl">Next Thursday - 8:30 PM</p>
+              <p className="text-gray-700 text-lg md:text-xl">Uris Hall - 3rd Floor</p>
+              <p className="text-lg md:text-xl text-gray-500 mt-2">
                 Join us for a night of Texas Hold'em. Free entry!
               </p>
             </div>
 
-            <div className="bg-white shadow-lg p-20 rounded-lg border border-gray-200">
-              <h3 className="text-2xl font-bold text-secondary mb-2">
+            <div className="bg-white shadow-lg p-10 md:p-20 rounded-lg border border-gray-200">
+              <h3 className="text-xl md:text-2xl font-bold text-secondary mb-2">
                 Tournament Qualifier
               </h3>
-              <p className="text-gray-700 text-xl">March 6th - 7:30 PM</p>
-              <p className="text-xl text-gray-500 mt-2">
+              <p className="text-gray-700 text-lg md:text-xl">March 6th - 7:30 PM</p>
+              <p className="text-lg md:text-xl text-gray-500 mt-2">
                 Online qualifier for March 30th tournament, top X competitors
                 qualify
               </p>
             </div>
 
-            <div className="bg-white shadow-lg p-20 rounded-lg border border-gray-200">
-              <h3 className="text-2xl font-bold text-secondary mb-2">
+            <div className="bg-white shadow-lg p-10 md:p-20 rounded-lg border border-gray-200">
+              <h3 className="text-xl md:text-2xl font-bold text-secondary mb-2">
                 Spring Tournament
               </h3>
-              <p className="text-gray-700 text-xl">
+              <p className="text-gray-700 text-lg md:text-xl">
                 Sunday, March 30th - Noon to 9pm
               </p>
-              <p className="text-xl text-gray-500 mt-2">
-                Compete in Columbia's largers poker tournament, 150 players in
+              <p className="text-lg md:text-xl text-gray-500 mt-2">
+                Compete in Columbia's largest poker tournament, 150 players in
                 the field, over $1000 in prizes from top hedge funds and quant
                 firms in the area.
               </p>
