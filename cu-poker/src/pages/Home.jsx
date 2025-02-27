c7820ae5f2c08dd2ada8c0a62a68ecf8da94f4d1
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2025-02-27 17:30:48 -0500

    new about page

--- cu-poker/src/pages/Home.jsx
+++ cu-poker/src/pages/Home.jsx
@@ -170,7 +170,7 @@ function Home() {
 
           <iframe
             src="https://calendar.google.com/calendar/embed?src=c_e9a2bda6adc3d9f883f7acc86756d418132483828382004ab9e93dd530e92f39%40group.calendar.google.com&ctz=America%2FNew_York"
-            className="block mx-auto mt-8"
+            className="block mx-auto mt-8 rounded-lg"
             style={{ border: 0, aspectRatio: '4/3', maxWidth: '800px' }}
             width="100%"
           ></iframe>
